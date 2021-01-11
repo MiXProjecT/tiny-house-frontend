7a8bcd09cec85887ddd9f4f5a2598910e3f2a0ef
Vladimir Shumkov <Vovan4ik112@gmail.com>
2021-01-11 19:23:46 +0500

    change custom hooks from useState to useReducer

--- src/lib/api/useMutation.ts
+++ src/lib/api/useMutation.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import server from "./server";
 
 interface State<TData> {
@@ -7,15 +7,37 @@ interface State<TData> {
   error: boolean;
 }
 
+type Action<TData> =
+  | { type: "FETCH" }
+  | { type: "FETCH_SUCCESS"; payload: TData }
+  | { type: "FETCH_ERROR" };
+
 type MutationTuple<TData, TVariables> = [
   (variables?: TVariables | undefined) => Promise<void>,
   State<TData>
 ];
 
+const reducer = <TData>() => (
+  state: State<TData>,
+  action: Action<TData>
+): State<TData> => {
+  switch (action.type) {
+    case "FETCH":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { data: action.payload, loading: false, error: false };
+    case "FETCH_ERROR":
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
+  }
+};
+
 const useMutation = <TData, TVariables = never>(
   query: string
 ): MutationTuple<TData, TVariables> => {
-  const [state, setState] = useState<State<TData>>({
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false,
@@ -23,7 +45,7 @@ const useMutation = <TData, TVariables = never>(
 
   const fetch = async (variables?: TVariables) => {
     try {
-      setState({ data: null, loading: true, error: false });
+      dispatch({ type: "FETCH" });
 
       const { data, errors } = await server.fetch<TData, TVariables>({
         query,
@@ -34,9 +56,9 @@ const useMutation = <TData, TVariables = never>(
         throw new Error(errors[0].message);
       }
 
-      setState({ data, loading: false, error: false });
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
     } catch (error) {
-      setState({ data: null, loading: false, error: true });
+      dispatch({ type: "FETCH_ERROR" });
       throw new Error(error);
     }
   };
