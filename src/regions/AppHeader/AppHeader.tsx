11930556ab3aeff5c59c733f85beae12a861ef7f
Vladimir Shumkov <Vovan4ik112@gmail.com>
2021-01-19 23:07:34 +0500

    add App header region

---
+++ src/regions/AppHeader/AppHeader.tsx
@@ -0,0 +1,76 @@
+import React, { useMemo } from "react";
+import { HomeOutlined, LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { Button, Avatar } from "antd";
+import { Viewer } from "lib/graphql/generated";
+import logo from "./assets/tinyhouse-logo.png";
+import {
+  Header,
+  Navigation,
+  Menu,
+  ItemLogo,
+  ItemHome,
+  ItemAuth,
+  UserLinksSubMenu,
+  LogoLink,
+  Logo,
+} from "./style";
+
+const { Item } = Menu;
+
+interface Props {
+  viewer: Viewer;
+}
+
+const AppHeader = ({ viewer }: Props): JSX.Element => {
+  const userAvatar = useMemo(() => <Avatar src={viewer.avatar} />, [
+    viewer.avatar,
+  ]);
+  const subMenuLogin = useMemo(
+    () =>
+      viewer.id ? (
+        <UserLinksSubMenu title={userAvatar}>
+          <Item key="user">
+            <UserOutlined />
+            Profile
+          </Item>
+          <Item key="logout">
+            <Link to="/logout">
+              <LogoutOutlined />
+              Log Out
+            </Link>
+          </Item>
+        </UserLinksSubMenu>
+      ) : (
+        <ItemAuth key="login">
+          <Link to="/login">
+            <Button type="primary">Sign In</Button>
+          </Link>
+        </ItemAuth>
+      ),
+    [viewer.id, userAvatar]
+  );
+
+  return (
+    <Header role="banner">
+      <Navigation>
+        <Menu mode="horizontal" selectable={false}>
+          <ItemLogo key="home">
+            <LogoLink to="/">
+              <Logo src={logo} />
+            </LogoLink>
+          </ItemLogo>
+          <ItemHome key="host">
+            <Link to="/host">
+              <HomeOutlined />
+              Home
+            </Link>
+          </ItemHome>
+          {subMenuLogin}
+        </Menu>
+      </Navigation>
+    </Header>
+  );
+};
+
+export default AppHeader;
---
+++ src/regions/AppHeader/style.ts
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import { Layout, Menu as AntdMenu } from "antd";
+import { Link } from "react-router-dom";
+
+const { Header: AntdHeader } = Layout;
+const { Item, SubMenu } = AntdMenu;
+
+export const Header = styled(AntdHeader)`
+  background: #fff;
+  box-shadow: 0 2px 8px #f0f1f2;
+  padding: 0;
+  z-index: 99;
+`;
+
+export const Navigation = styled.nav`
+  display: grid;
+`;
+
+export const LogoLink = styled(Link)`
+  display: inline-block;
+  padding: 0 20px;
+`;
+
+export const Logo = styled.img`
+  width: 36px;
+`;
+
+export const Menu = styled(AntdMenu)`
+  width: 100%;
+  padding: 0 20px;
+  border: 0;
+  display: grid;
+  grid-template-areas: "logo search home auth";
+  grid-auto-columns: auto minmax(0, 1fr) auto auto;
+  &:before {
+    content: none;
+  }
+`;
+
+export const ItemLogo = styled(Item)`
+  grid-area: logo;
+
+  &&&:hover {
+    color: transparent;
+    border-bottom: none;
+  }
+`;
+
+export const ItemHome = styled(Item)`
+  grid-area: home;
+`;
+
+export const ItemAuth = styled(Item)`
+  grid-area: auth;
+`;
+
+export const UserLinksSubMenu = styled(SubMenu)`
+  grid-area: auth;
+`;
