eeaa1064502d4315f024660e015f0ed3c1f40477
Vladimir Shumkov <Vovan4ik112@gmail.com>
2021-01-19 23:09:27 +0500

    add App header region

---
+++ src/sections/LogOut/LogOut.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { Spin } from "antd";
+import { Redirect } from "react-router-dom";
+import { Viewer, useLogOutMutation } from "lib/graphql/generated";
+import { displayErrorMessage, displaySuccessNotification } from "utils";
+import { LogOutSection } from "./style";
+
+interface Props {
+  setViewer: (viewer: Viewer) => void;
+}
+
+const LogOut = ({ setViewer }: Props): JSX.Element => {
+  const [logOut, { data: logOutData }] = useLogOutMutation({
+    onCompleted: (data) => {
+      if (data?.logOut) {
+        setViewer(data.logOut);
+        displaySuccessNotification("You've successfully logged out!");
+      }
+    },
+    onError: () => {
+      displayErrorMessage("Something went wrong! Please try again :(");
+    },
+  });
+
+  useEffect(() => {
+    logOut();
+  }, [logOut]);
+
+  return logOutData?.logOut ? (
+    <LogOutSection>
+      <Spin size="large" />{" "}
+    </LogOutSection>
+  ) : (
+    <Redirect to="/" />
+  );
+};
+
+export default LogOut;
---
+++ src/sections/LogOut/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Layout } from "antd";
+
+const { Content } = Layout;
+
+export const LogOutSection = styled(Content)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: auto;
+`;
